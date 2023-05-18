ec0fd11d89df80db0b334e4306a4046eff74169e
CodeForGhost <ilikeabdul7@gmail.com>
2023-05-18 12:33:12 +0530

    finished

--- book-ui/src/components/home/Home.js
+++ book-ui/src/components/home/Home.js
@@ -1,73 +1,90 @@
-import React, { Component } from 'react'
-import { Statistic, Icon, Grid, Container, Image, Segment, Dimmer, Loader } from 'semantic-ui-react'
-import { bookApi } from '../misc/BookApi'
-import { handleLogError } from '../misc/Helpers'
+import React, { Component } from "react";
+import {
+  Statistic,
+  Icon,
+  Grid,
+  Container,
+  Image,
+  Segment,
+  Dimmer,
+  Loader,
+} from "semantic-ui-react";
+import { bookApi } from "../misc/BookApi";
+import { handleLogError } from "../misc/Helpers";
 
 class Home extends Component {
   state = {
     numberOfUsers: 0,
     numberOfBooks: 0,
-    isLoading: false
-  }
+    isLoading: false,
+  };
 
   async componentDidMount() {
-    this.setState({ isLoading: true })
+    this.setState({ isLoading: true });
     try {
-      let response = await bookApi.numberOfUsers()
-      const numberOfUsers = response.data
+      let response = await bookApi.numberOfUsers();
+      const numberOfUsers = response.data;
 
-      response = await bookApi.numberOfBooks()
-      const numberOfBooks = response.data
+      response = await bookApi.numberOfBooks();
+      const numberOfBooks = response.data;
 
-      this.setState({ numberOfUsers, numberOfBooks })
+      this.setState({ numberOfUsers, numberOfBooks });
     } catch (error) {
-      handleLogError(error)
+      handleLogError(error);
     } finally {
-      this.setState({ isLoading: false })
+      this.setState({ isLoading: false });
     }
   }
 
   render() {
-    const { isLoading } = this.state
+    const { isLoading } = this.state;
     if (isLoading) {
       return (
         <Segment basic style={{ marginTop: window.innerHeight / 2 }}>
           <Dimmer active inverted>
-            <Loader inverted size='huge'>Loading</Loader>
+            <Loader inverted size="huge">
+              Loading
+            </Loader>
           </Dimmer>
         </Segment>
-      )
+      );
     } else {
-      const { numberOfUsers, numberOfBooks } = this.state
+      const { numberOfUsers, numberOfBooks } = this.state;
       return (
         <Container text>
           <Grid stackable columns={2}>
             <Grid.Row>
-              <Grid.Column textAlign='center'>
-                <Segment color='blue'>
+              <Grid.Column textAlign="center">
+                {/* <Segment color='blue'>
                   <Statistic>
                     <Statistic.Value><Icon name='user' color='grey' />{numberOfUsers}</Statistic.Value>
                     <Statistic.Label>Users</Statistic.Label>
                   </Statistic>
-                </Segment>
+                </Segment> */}
               </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <Segment color='blue'>
+              <Grid.Column textAlign="center">
+                {/* <Segment color='blue'>
                   <Statistic>
                     <Statistic.Value><Icon name='book' color='grey' />{numberOfBooks}</Statistic.Value>
                     <Statistic.Label>Books</Statistic.Label>
                   </Statistic>
-                </Segment>
+                </Segment> */}
               </Grid.Column>
             </Grid.Row>
           </Grid>
 
-          <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          {/* <Image
+            src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png"
+            style={{ marginTop: "2em" }}
+          />
+          <Image
+            src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+            style={{ marginTop: "2em" }}
+          /> */}
         </Container>
-      )
+      );
     }
   }
 }
 
-export default Home
+export default Home;
